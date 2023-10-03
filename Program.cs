c71765c760adbe301e36a25b2a8bcb4d262dbbf3
Jacob Lundström <113439800+JacobLundstrom@users.noreply.github.com>
2023-10-03 23:34:47 +0200

    implemented ad calculations

--- Program.cs
+++ Program.cs
@@ -11,6 +11,12 @@ class Program
         Console.Write("Ange försäljningspriset för produkten inklusive moms: ");
         double försäljningsprisInklMoms = double.Parse(Console.ReadLine());
 
+        Console.Write("Ange marknadsföringskostnaden per dag: ");
+        double marknadsföringskostnadPerDag = double.Parse(Console.ReadLine());
+
+        //Console.Write("Ange antal sålda produkter per dag (0 om du vill räkna ut break-even): ");
+        //int såldaProdukterPerDag = int.Parse(Console.ReadLine());
+
         // Beräkna kostnaden exklusive moms
         double momsProcent = 25.0; // 25% moms
         double kostnadExklMoms = inköpsprisInklMoms / (1 + (momsProcent / 100));
@@ -28,24 +34,37 @@ class Program
         // Beräkna vinsten inklusive Klarnas avgift
         double vinstMedAvgift = vinstExklMoms - klarnaAvgift;
 
+        //// Beräkna marknadsföringskostnaden
+        //double totalMarknadsföringskostnad = marknadsföringskostnadPerDag * såldaProdukterPerDag;
+
         // Skattesats (40% skatt)
         double skattesats = 40.0; // 40% skatt
         double skatt = vinstExklMoms * (skattesats / 100);
 
-        Console.WriteLine($"\nKostnaden utan moms är: {kostnadExklMoms} kr\n");
-        Console.WriteLine($"Intäkten utan moms är: {intäktExklMoms} kr\n");
+        Console.WriteLine($"\nKostnaden utan moms är: {kostnadExklMoms} kr");
+        Console.WriteLine($"Intäkten utan moms är: {intäktExklMoms} kr");
+        Console.WriteLine($"Klarnas avgift är: {klarnaAvgift} kr");
+        Console.WriteLine($"Vinsten exklusive moms är: {vinstExklMoms} kr");
+        Console.WriteLine($"Skatten är: {skatt} kr");
+        Console.WriteLine($"Vinsten inklusive Klarnas avgift är: {vinstMedAvgift} kr");
+        Console.WriteLine($"Marknadsföringskostnaden är: {marknadsföringskostnadPerDag} kr");
 
-        Console.WriteLine($"Klarnas avgift är: {klarnaAvgift} kr\n");
+        //if (såldaProdukterPerDag == 0)
+        //{
+        // Räkna ut antal produkter som behöver säljas för break-even
+        int produkterFörBreakEven = (int)Math.Ceiling(marknadsföringskostnadPerDag / vinstMedAvgift);
 
-        Console.WriteLine($"Vinsten exklusive moms är: {vinstExklMoms} kr\n");
-        Console.WriteLine($"Skatten är: {skatt} kr\n");
-        Console.WriteLine($"Vinsten inklusive Klarnas avgift är: {vinstMedAvgift} kr\n");
+        // Räkna ut den totala vinsten efter marknadsföring
+        double totalVinstEfterMarknadsföring = produkterFörBreakEven * vinstMedAvgift - marknadsföringskostnadPerDag;
+        Console.WriteLine($"\nDu måste sälja minst {produkterFörBreakEven} st per dag för att inte gå back");
+        Console.WriteLine($"Eftersom 1 produkt blir {vinstMedAvgift} kr och ads per dag är {marknadsföringskostnadPerDag} kr vilket blir {totalVinstEfterMarknadsföring} kr över 0kr sträcket!");
+        Console.WriteLine($"\nTotala vinsten efter marknadsföring: {totalVinstEfterMarknadsföring} kr");
+        Console.WriteLine($"\nTotala vinsten per person blir: {totalVinstEfterMarknadsföring / 2} kr");
+        
+        
 
-        double exaktVinst = vinstMedAvgift - skatt;
 
-        Console.WriteLine($"Den exakta vinsten med allt inräknat är: {exaktVinst} kr\n");
-
-        Console.WriteLine("Tryck på valfri tangent för att avsluta...");
+        Console.WriteLine("\nTryck på valfri tangent för att avsluta...");
         Console.ReadKey();
     }
 }
